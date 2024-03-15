4bce43170b108049d91883cc6c87c147fad9abdc
Catalin <feiercatalin123@icloud.com>
2024-03-15 11:16:17 +0200

    Regex fix + toString method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         Polynomial polynomial1=new Polynomial();
-        String input1 = "3x^2 - x^3 + 2x + 1";
+        String input1 = "x+1";
         polynomial1.readPolynomial(input1);
         polynomial1.printPolynomial();
     }
